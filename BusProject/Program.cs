d04d9edda682fb0016a930680072b508180a9175
dinaEldesoky <45181386+dinaEldesoky@users.noreply.github.com>
2020-11-16 21:50:23 +0200

    eduh

--- BusProject/Program.cs
+++ BusProject/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,24 @@ namespace BusProject
     {
         static void Main(string[] args)
         {
+
+            RideTrip rt = new RideTrip();
+
+            Console.WriteLine("Please enter the Start station and its number : ");
+            string startPoint = Console.ReadLine();
+            int numOfStartPoint = int.Parse(Console.ReadLine());
+            rt.setStartPoint(startPoint);
+            rt.setnumOfStartStation(numOfStartPoint);
+            Console.WriteLine("Please enter the End station and its number :");
+            string endPoint = Console.ReadLine();
+            int numOfEndPoint = int.Parse(Console.ReadLine());
+            rt.setEndPoint(endPoint);
+            rt.setnumOfEndStation(numOfEndPoint);
+            Console.WriteLine("The Trip cost is: {0}", rt.cost(rt.getnumOfStartStation(), rt.getnumOfEndStation()));
+
+
+
+            Console.ReadKey();
         }
     }
 }
---
+++ BusProject/RideTrip.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusProject
+{
+    class RideTrip
+    {
+        private string startPoint;
+        private string endPoint;
+        private int numOfStartStation;
+        private int numOfEndStation;
+
+        public void setStartPoint(string startPoint)
+        {
+            this.startPoint = startPoint;
+        }
+        public string getStartPoint()
+        {
+            return startPoint;
+        }
+
+        public void setnumOfStartStation(int numOfStartStation)
+        {
+            this.numOfStartStation = numOfStartStation;
+        }
+        public int getnumOfStartStation()
+        {
+            return numOfStartStation;
+        }
+
+        public void setnumOfEndStation(int numOfEndStation)
+        {
+            this.numOfEndStation = numOfEndStation;
+        }
+        public int getnumOfEndStation()
+        {
+            return numOfEndStation;
+        }
+
+        public void setEndPoint(string endPoint)
+        {
+            this.endPoint = endPoint;
+        }
+        public string getEndPoint()
+        {
+            return endPoint;
+        }
+
+        public int cost(int numOfStartStation, int numOfEndStation)
+        {
+            int Cost = Math.Abs((numOfStartStation - numOfEndStation) * 2);
+
+            return Cost;
+        }
+
+    }
+}
