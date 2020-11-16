eba6a4fd4b6273485d526a1da0810b8d768b2d5d
OlaAwad <ola.khireldin@gmail.com>
2020-11-16 22:44:51 +0200

    OOO

--- BusProject/Route.cs
+++ BusProject/Route.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace BusProject
 {
     class Route
-    {
+    {      
+            int numberOfStations = 7;
+            public string[] line1 = { "Helwan", "Helwan University", "Maadi", "Al-Sadat", "Demerdash", "Ain Shams", "Elmarg" };
+            //  public string[] line2 = { "Monib", "Giza", "Cairo University", "Dokki", "Muhammed Nagib", "Santa Tresa", "Shoubra" };
+            // public string[] line3 = { "Airport", "Elnozha", "Cairo stadium", "Abdo Basha", "Nasser", "Zamalek", "Mostafa Mahmoud mosque" };
+                  
     }
 }
