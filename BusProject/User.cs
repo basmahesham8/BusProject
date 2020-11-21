8eb6492e4bbddfd8f7e41708a422d4340bee8f6b
basmahesham8 <45381105+basmahesham8@users.noreply.github.com>
2020-11-21 18:19:25 +0200

    hello from basma

--- BusProject/User.cs
+++ BusProject/User.cs
@@ -8,5 +8,46 @@ namespace BusProject
 {
     class User
     {
+        static public int userID = 0;
+        public double userCredit { set; get; }
+        enum userType { 0:elders,1:youth,2:kids under 5}
+
+
+        public string userName { set; get; }
+        int userAge { set; get; }
+        string usreContact { set; get; }
+
+
+        public User(string name, int age, string contact)
+        {
+
+            if (age < 5)
+                userType = 2;
+            else if (age > 60)
+                userType = 0;
+            else userType = 1;
+            this.userName = name;
+            this.usreContact = contact;
+        }
+        public User()
+        {
+            if (this.userAge < 5)
+                userType = 2;
+            else if (this.userAge > 60)
+                userType = 0;
+            else userType = 1;
+        }
+        public static User register(string name, int age, string contact)
+        {
+            User c;
+            c.userAge = age;
+
+            c.userName = name;
+            c.usreContact = contact;
+
+            return c;
+        }
+
+
     }
 }
